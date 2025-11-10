afe30fa9e54fdd00106e02f827a497d00f364a27
kathrinmzl <kathrin@muenzel.de>
2025-11-10 16:19:59 +0100

    chore: make load_default_data distinguish between cleaning or not cleaning the data

--- src/data_management.py
+++ src/data_management.py
@@ -6,12 +6,15 @@ from feature_engine.outliers import Winsorizer
 from feature_engine.imputation import MeanMedianImputer
 
 @st.cache_data
-def load_default_data():
+def load_default_data(clean=False):
     df = pd.read_csv("outputs/datasets/collection/LoanDefaultData.csv")
-    imputer = MeanMedianImputer(imputation_method='median', variables=['person_emp_length', 'loan_int_rate'])
-    df = imputer.fit_transform(df)
-    winsorizer = Winsorizer(capping_method='iqr', fold=5, tail='right', variables=["person_income","person_emp_length"])
-    df = winsorizer.fit_transform(df)
+    
+    if clean:
+        imputer = MeanMedianImputer(imputation_method='median', variables=['person_emp_length', 'loan_int_rate'])
+        df = imputer.fit_transform(df)
+        winsorizer = Winsorizer(capping_method='iqr', fold=5, tail='right', variables=["person_income","person_emp_length"])
+        df = winsorizer.fit_transform(df)
+        
     return df
 
 
